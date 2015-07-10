1bcb5b698b3209cc39d4930067d5a410f60e6328
Elvil <sil.benoit@gmail.com>
2015-07-10 15:37:43 +0200

    Finish toy app

--- hello_app/db/schema.rb
+++ hello_app/db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709185045) do
+ActiveRecord::Schema.define(version: 20150710132021) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
-    t.integer  "users_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
